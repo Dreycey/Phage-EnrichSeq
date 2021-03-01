7a14053499e0472a8a4811fe1604818e820f2645
Dreycey <albindreycey@gmail.com>
2021-02-28 20:39:06 -0700

    updating kraken_db_format

--- downloadPhageGenomes_module/kraken_db_format.py
+++ downloadPhageGenomes_module/kraken_db_format.py
@@ -1,6 +1,6 @@
 import sys
 from re import search
-
+from tqdm import tqdm
 
 """
 v2021.02.11
@@ -13,32 +13,29 @@ PREREQUISITE:
 * Must have run kraken2-build --download-taxonomy --db <DBNAME>. names.dmp file will be in DBNAME/taxonomy/
 
 USAGE:
-python kraken_db_format.py <NCBI names.dmp file> <DB FASTA file to be formatted>
-e.g. python kraken_db_format.py names.dmp Actinobacteriophages-All.fasta
+python kraken_db_format.py <NCBI names.dmp file> <DB FASTA file to be formatted> <Kraken outfile>
+e.g. python kraken_db_format.py names.dmp Actinobacteriophages-All.fasta phageDB_out.fa
 """
 
 def parseIntoDictionary(dmp_file):
     """ parse file into data structure"""
     dict_tx_names = {}
-    for line in dmp_file:
+    for line in tqdm(dmp_file):
         line = line.strip("\n").replace('\t', '').split("|")
         taxid = line[0]
         species_name = line[1]
         line_type  = line[3]
-        if "perseus" in line:
-            print(line_type)
-        if "scientific" in line_type and "phage" in species_name:
+
+        if "scientific" in line_type and "phage" in species_name: 
+            dict_tx_names[species_name] = taxid   
+        elif "scientific" in line_type and "virus" in species_name: 
             dict_tx_names[species_name] = taxid
 
     return dict_tx_names
 
 def findSpeciesId(tax_dict, name):
     """ finds the taxid """
-    #return tax_dict[name]
     for key in tax_dict.keys():
-        # below could be improved using regex
-        #if key.lower().find(name.lower()) != -1:
-        #if search(name.lower().replace(" ", ""), key.lower().replace(" ", "")):
         if search(name.lower(), key.lower()):
             return tax_dict[key]
 
@@ -46,36 +43,31 @@ def findSpeciesId(tax_dict, name):
     return 0
 
 
-def addTaxIdToFile(tax_dict, fasta_file):
+def addTaxIdToFile(tax_dict, fasta_file, outfile):
     """ adds taxid to the phagesDB fasta file for kraken2 format """
     fasta_file_contents = open(fasta_file).readlines()
-    #names_file_contents = open(names_file).readlines() #list
 
     # new DB file to add taxon id to
-    filename = "phagesDB.fasta"
-    taxonfile = open(filename, "w")
+    taxonfile = open(outfile, "w")
 
-    for line in fasta_file_contents:
+    nonfound_count = 0 #counting number of names not found in names.dmp
+    for line in tqdm(fasta_file_contents):
         line = line.strip("\n") # will this affect the final file format when using in kraken?
         if '>' in line:
-            species_name = line.split(' ')[2]
+            species_name = line.split(' ')[2].strip(",")
             taxid = findSpeciesId(tax_dict, species_name)
-            phage_name_line = line.split()
-
-            #print(f'Name: {species_name}, taxid: {taxid}') #works
-            #newline = line[0] + line[1] + "|kraken:taxid|" + str(taxid)
-
-            newline = ' '.join(phage_name_line[:3:]) + "|kraken:taxid|" + str(taxid) + " " + ' '.join(phage_name_line[3::])
-            #print(newline)
-            # write this modified line to the new file
-            taxonfile.write(newline + "\n")
+            if taxid != 0:
+                phage_name_line = line.split()
+                newline = ' '.join(phage_name_line[:3:]) + "|kraken:taxid|" + str(taxid) + " " + ' '.join(phage_name_line[3::])
+                taxonfile.write(newline + "\n")
+            else:
+                nonfound_count += 1
+                continue
         else:
-            # write the rest of the nucleotide info into the new file (unchanged)
             taxonfile.write(line + "\n")
-    #print(fasta_file_contents[100].split(' ')[2]) # prints the phage name
 
     taxonfile.close()
-    return filename
+    return outfile, nonfound_count
 
 
 def main():
@@ -84,25 +76,17 @@ def main():
     ### SCRIPT INPUT
     dmp_file_path = sys.argv[1]
     db_file_path = sys.argv[2]
+    outfile = sys.argv[3]
 
     ### SCRIPT
     file_name = open(dmp_file_path).readlines()
     tax_map = parseIntoDictionary(file_name)
 
-
-    # FIND NAMES
-    #name_file = open(line_sep_path).readlines()
-
-    # for species_name in name_file:
-    #     species_name = str(species_name).strip("\n")
-    #     taxid = findSpeciesId(tax_map, species_name)
-        #print(species_name, taxid)
-
     # MODIFY DB FILE (ADD TAXON ID)
     print('Creating kraken-compatible database file...')
-    f = addTaxIdToFile(tax_map, db_file_path)
+    f,outnon = addTaxIdToFile(tax_map, db_file_path, outfile)
     print(f'File created: {f}')
-
+    print(f"{outnon} phages did not have tax mappings")
 
 if __name__ == "__main__":
     main()
