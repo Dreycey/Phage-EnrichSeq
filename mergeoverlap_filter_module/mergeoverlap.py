a4b2ac186eb2af9ccad6d88784e844ea67a635c1
Dreycey <albindreycey@gmail.com>
2021-09-29 19:07:41 -0600

    merge overlap filter is now working at a basic capacity, still need to add a KNN selector

--- mergeoverlap_filter_module/mergeoverlap.py
+++ mergeoverlap_filter_module/mergeoverlap.py
@@ -23,10 +23,13 @@ from typing import Dict, List
 import csv
 import os
 import argparse
+import numpy as np
 # non-std packages
 import mappy as mp
 import matplotlib.pyplot as plt
 # in house packages
+sys.path.append("/Users/dreyceyalbin/Desktop/Phage-EnrichSeq/PathOrganizer_module")
+from PathOrganizer import PathOrganizer, PathErrors, DuplicateGenomeError
 
 
 
@@ -52,12 +55,10 @@ def parseArgs(argv=None) -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__)
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-v", "--verbose", action="store_true")
-    group.add_argument("-q", "--quiet", action="store_true")
-    parser.add_argument("-i", "--config", help="input config file", required=True)
-    parser.add_argument("-c", "--coverage", help="coverage for output", required=True)
-    parser.add_argument("-o", "--output", help="the output file prefix", required=True)
+    parser.add_argument("-i", "--input", help="the input lsv file with phage names", required=True)
+    parser.add_argument("-o", "--output_prefix", help="the output file prefix", required=True)
+    parser.add_argument("-f", "--fasta", help="input fasta path", required=True)
     parser.add_argument("-t", "--threads", help="number of threads", required=True)
-    parser.add_argument("-rn", "--readnum", help="number of reads to simulate", required=True)
     return parser.parse_args(argv)
 
 class GenomeMapper(ABC):
@@ -98,6 +99,7 @@ class MinimapMapperWithInfo(GenomeMapper):
     def __init__(self, name, file_path):
         self._genome_name = name
         self.index_object = mp.Aligner(file_path, best_n=1)
+        self.minimap_out = {}
 
     def map_fasta_read(self, sequence):
         """
@@ -113,7 +115,7 @@ class MinimapMapperWithInfo(GenomeMapper):
         """
         try:
             first_result = next(self.map_fasta_read(sequence))
-            if self.genome_name in first_result:
+            if self.genome_name in self.minimap_out:
                 self.minimap_out[self.genome_name]["mapq"].append(first_result.mapq)
                 #self.minimap_out[genomeName]["readmaps"].add((first_result.r_st, first_result.r_en))
                 self.minimap_out[self.genome_name]["readmaps"].append((first_result.r_st, first_result.r_en))
@@ -152,99 +154,48 @@ class MinimapMapper(GenomeMapper):
             return False
         return True
 
+
+
+
+
+
+
+
+
+
 class GenomeTestSet:
     """
     This class is used to test the a folder of simulated sequences
     """
     
-    def __init__(self):
+    def __init__(self, line_seperated_genomes,genome_directory):
         """ initialize all params """
+        # input attributes
+        self.input_genomes = [genome_name.strip("\n").split(" ")[2] 
+                              for genome_name in open(line_seperated_genomes).readlines()]
+        self.object_PathOrganizer = PathOrganizer(genome_directory)
+        # primary attributes
         self.genomes = {} # this directory will hold all of the genomes
-        self.genomeMap: Dict[str,MinimapMapper] = {} # this dictionary hold minmap indexes
+        self.genomeMap: Dict[str, MinimapMapper] = {} # this dictionary hold minmap indexes
         self.simAbundance = {} # this dictionary hold simulated abundance amounts
         # add genomes
         self.__addGenomes()
         # keep results saved
         self.resultDict = {} #if empty, can't plot
-        # new features
         self.minimap_out = {} # this holds info yeilded from minimap2
-        self.genomes_to_view = self.parseGenomeList(genomes_to_view) # this will hold the genome names parse
 
     def __addGenomes(self):
         """ add genomes to to genomes attr """
-        #  parse multifasta  (ASSUMES A GIANT MULTIFASTA FILE)
         print("Creating indexes for minimap2")
-        genomeName_list, genome_list = self.parseFasta(self.configFilePath) #TODO: FIX
-        for indx in tqdm(range(len(genomeName_list))):
-            genome = genome_list[indx]
-            genomeName = genomeName_list[indx]
-            kracken_out_list = ["Ryadel", "Blessica", "D29", "Paphu", "Perseus"]
-            for name in kracken_out_list:
-                if  name in genomeName: #in ["Ryadel", "Blessica", "D29", "Paphu", "Perseus"]:
-                    print(f"genome name: {genomeName}")
-                    # reformat
-                    genome_name = genomeName.strip("\n").split(",")[0]
-                    genome = genome.strip("\n")
-                    # add to the genomes dict
-                    self.genomes[genome_name] = genome
-                    # create a tempory file for mappy
-                    tempfile = open(genome_name+".fa","w")
-                    tempfile.write(">"+genome_name+"\n"+genome)
-                    # create minimap object
-                    self.genomeMap[genome_name] = MinimapMapperWithInfo(name=genome_name, 
-                                                                        file_path=genome_name+".fa")
-                    # delete the temp file
-                    tempfile.close()
-                    #os.remove("tempfileDELETE.fa")
-    
-    def parseGenomeList(self, input_file=None):
-        """ parses names seperate by line """
-        if (input_file != None):
-            genomes_to_view = []
-            file_lines = open(input_file).readlines()
-            for line in file_lines:
-                line = line.strip("\n")
-                if len(line) > 0:
-                    genomes_to_view.append(line)
-            return genomes_to_view
-        return self.genomes_to_view
-
-    def print_minimap2output(self):
-        """ prints output in minimap_out datastructure """
-
-        for genomeName in self.minimap_out:
-            print(genomeName)
-            avg_mapq = np.average(self.minimap_out[genomeName]["mapq"])
-            print(f"average map quality: {avg_mapq}")
-            readcount = self.minimap_out[genomeName]["readcount"]
-            print(f"read count: {readcount}")
-            #plt.hist(self.minimap_out[genomeName]["mapq"])
-            #plt.show()
-            overlappMerge = self.overlapMerge(genomeName)
-            percenOverlap = self.calcGenomePercetage(genomeName, overlappMerge)
-            print(f"percent overlap: {percenOverlap}")
-
-    def __findSeq(self, input_seq):
-        """ 
-        INPUT 
-            1. input read
-        OUTPUT
-            which genome
-            if no genome matching OR more than one, output null 
-        """
-        genome_from = ""
-        # find genome that read is in
-        for genome_name in self.genomes.keys():
-            minimap_mapper: MinimapMapper = self.genomeMap[genome_name]
-            read_maps_to_genome: bool = minimap_mapper.does_read_map(input_seq)
-            if read_maps_to_genome:
-                if (genome_from == ""):
-                    return genome_name
-                else:
-                    return "MULTIPLE"
-        if genome_from == "":  # if nothing, return UNK
-            return "UNK"
-        return genome_from
+        for genome_to_grab in self.input_genomes:
+            print(f"genome name: {genome_to_grab}")
+            file_path = self.object_PathOrganizer.genome(genome_to_grab)
+            if file_path != None: # TODO: IF THIS IS NONE THEN THERE'S A PROBLEM FINDING GENOMES!!
+                self.genomes[genome_to_grab] = self.parseFasta(file_path)[1][0]
+                self.genomeMap[genome_to_grab] = MinimapMapperWithInfo(name=genome_to_grab,
+                                                            file_path=str(file_path))
+            else:
+                print(f"FIX THIS: there's a problem finding the genome for {genome_to_grab}")
 
     @staticmethod
     def parseFasta(fasta_path):
@@ -254,6 +205,7 @@ class GenomeTestSet:
         OUTPUT - 1. name (ordered list); 2. genome sequence (ordered list)
         """
         seq_names, sequences = [], []
+        print(f"fasta file: {fasta_path}")
         with open(fasta_path) as fasta_file:
             fasta_input = fasta_file.readlines()
             sequence_i = ""
@@ -267,7 +219,7 @@ class GenomeTestSet:
                 else:
                     sequence_i += line.strip("\n")
             sequences.append(sequence_i.strip("\n"))
-        return seq_names, sequences        
+        return seq_names, sequences     
 
     def checkSeqFile(self, input_fasta):
         """
@@ -284,8 +236,11 @@ class GenomeTestSet:
         # normalize the results
         for genome in genome_count.keys():
             genome_count[genome] = genome_count[genome] / total_reads
-        # return dictionary
         self.resultDict = genome_count
+        # return dictionary
+        for minimap_obj in self.genomeMap.values():
+            for gen, minimap_output in minimap_obj.minimap_out.items():
+                self.minimap_out[gen] = minimap_output
         return genome_count
 
     def count_reads_mapping_per_genome(self, seqs) -> Dict[str,int]:
@@ -301,6 +256,111 @@ class GenomeTestSet:
             else:
                 genome_count[genome] = 1
         return genome_count
+
+    def __findSeq(self, input_seq):
+        """ 
+        INPUT 
+            1. input read
+        OUTPUT
+            which genome
+            if 1. no genome matching, output 'UNK'.
+               2. more than one, output 'MULTIPLE'.
+               3. exactly one, output the name of the corresponding genome.
+        """
+        genome_from = ""
+        # find genome that read is in
+        for genome_name in self.genomes.keys():
+            minimap_mapper: MinimapMapper = self.genomeMap[genome_name]
+            read_maps_to_genome: bool = minimap_mapper.does_read_map(input_seq)
+            if read_maps_to_genome:
+                if (genome_from == ""):
+                    return genome_name
+                else:
+                    return "MULTIPLE"
+        if genome_from == "":  # if nothing, return UNK
+            return "UNK"
+        return genome_from
+
+    def overlapMerge(self, genome_name):
+        """ extend reads to assess % genome covered
+
+        Algorithm:
+            Example:
+                1. input
+                (50,150), (0,100), (200,300), (149,249)
+                2. sort by starting reference index
+                (0,100), (50,150), (149,249), (200,300)
+                3. merge
+                    iteration one:
+                        p1 = (0,100)
+                        p2 = (50, 150)
+                        if p1[1] >= p2[0]:
+                            merge
+                            p1 = merged
+                            p2 = move forward
+                        else:
+                            p1 = p2
+                            p2 = move forward
+
+        INPUT:
+            self.minimap_out[genomeName]["readmaps"] (set object)
+        OUTPUT:
+            return
+                1. merged set
+                2. % of genome with reads mapped
+        """
+        mergedSet = set()
+        #for genome_name in self.genomes.keys():
+        # run some type sorting algorithm
+
+        #1. input
+        mapped_reads = self.minimap_out[genome_name]["readmaps"] #[(50,120),(110,220),(150,200)]
+        print(f"before merge overlap: {len(mapped_reads)}")
+        #2. sort the reads by starting index
+        sorted_mapped_reads = sorted(mapped_reads, key=lambda x: x[0])
+
+        #3. merge overlapping regions
+        p1 = 0 # pointer 1
+        p2 = 1 # pointer 2
+        p1_start, p1_end = sorted_mapped_reads[p1]
+        while p2 < len(sorted_mapped_reads):
+            p2_start, p2_end = sorted_mapped_reads[p2]
+            if p1_end >= p2_start:  # if overlap,  then merge
+                if p1_end <= p2_end:
+                    p1_end = p2_end
+            else:                   # if no overlap, set p1=p2 and p2++
+                mergedSet.add((p1_start, p1_end))
+                p1_start, p1_end = sorted_mapped_reads[p2] #notice: using p2
+
+            p2 += 1
+        # add last interval to to the merged set
+        mergedSet.add((p1_start, p1_end))
+
+        print(f"after merge overlap: {len(mergedSet)}")
+        return mergedSet
+
+    def calcGenomePercetage(self, genome_name, merged_set):
+        """
+        This method calculate the percentage of the genome
+        covered by the overlapping intervals after merging.
+
+        input
+            overlap merge set
+        output
+            percentage of overlap for the mergedset
+        """
+        # initialize
+        overall_map_length = 0
+        genome_length = len(self.genomes[genome_name])
+        # calculate # of bases with mapped reads
+        for interval in merged_set:
+            interval_size = abs(interval[0] - interval[1])
+            overall_map_length += interval_size
+        # calculate % of genome with mapped reads
+        print(f"overlapp length: {overall_map_length}")
+        overlap_percent = overall_map_length / genome_length
+
+        return overlap_percent
 
     def plotResult(self, plotTitle, out=None, result=None):
         """
@@ -327,7 +387,7 @@ class GenomeTestSet:
         # save the plot
         plt.savefig(out, dpi=300)
 
-    def saveResultsAsCSV_1(self, csvout="testing.csv"):
+    def saveResultAsCSV(self, csvout="testing.csv"):
         """
         returns a csv that can be used in regression testing.
         """
@@ -336,32 +396,39 @@ class GenomeTestSet:
                 writer.writeheader()
                 writer.writerow(self.resultDict)
 
-    def saveResultAsCSV_2(self, outfilename):
-        """
-        Save the output as a CSV file.
-        """
-        if len(self.resultDict.keys()) == 0:
-            print("must create a result by running checkSeqFile")
-            exit(1)
-        # write to a CSV
-        with open(outfilename, 'w', newline='') as csvfile:
-            fieldnames = ['genome name', 'predicted abundance']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for genome_name, abundance in self.resultDict.items():
-                writer.writerow({'genome name': genome_name,
-                                 'predicted abundance': abundance})
+    def print_minimap2output(self, out_prefix):
+        """ prints output in minimap_out datastructure """
+        for genomeName in self.minimap_out:
+            print(genomeName)
+            avg_mapq = np.average(self.minimap_out[genomeName]["mapq"])
+            print(f"average map quality: {avg_mapq}")
+            readcount = self.minimap_out[genomeName]["readcount"]
+            print(f"read count: {readcount}")
+            plt.hist(self.minimap_out[genomeName]["mapq"])
+            plt.title(f"mapq Histogram for {genomeName}")
+            plt.savefig(out_prefix+"_"+genomeName+".png", dpi=300)
+            plt.clf()
+            overlappMerge = self.overlapMerge(genomeName)
+            percenOverlap = self.calcGenomePercetage(genomeName, overlappMerge)
+            print(f"percent overlap: {percenOverlap}")
 
 def main():
-    """ Runs the testing script """
-    # input
-    config_path = sys.argv[1]
-    fastaFile = sys.argv[2]
-    # Create object
-    config_object = GenomeTestSet(config_path) 
-    config_object.checkSeqFile(fastaFile) 
-    config_object.plotResult("Simulation Validation", out="simTest44.png")
-    config_object.saveResultsAsCSV()
+    print("RUNNING THE MERGE OVERLAP FILTER")
+    arguments = parseArgs(argv=sys.argv[1:])
+
+    #TESTING.
+    output_testing = open(arguments.output_prefix+"_testingArguments", "w")
+
+    # Running the algorithm.
+    GENOME_DIR = "/Users/dreyceyalbin/Desktop/Phage-EnrichSeq/kraken_module/ref_genomes/"
+    genomeTestObj = GenomeTestSet(line_seperated_genomes=arguments.input, genome_directory=GENOME_DIR)
+    genomeTestObj.checkSeqFile(arguments.fasta)
+    genomeTestObj.plotResult("Estimated Abundances Using Raw Read Mapping", out=arguments.output_prefix+".png")
+    genomeTestObj.saveResultAsCSV(arguments.output_prefix+".csv")
+    genomeTestObj.print_minimap2output(arguments.output_prefix)
+
+    # save to file
+    output_testing.write("RESULT: \n"+str(genomeTestObj.resultDict))
 
 if __name__ == "__main__":
     main()
