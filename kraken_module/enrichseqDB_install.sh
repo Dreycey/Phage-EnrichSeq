f945528751677174b2c96c217e9f4dcd0d4d2fba
Dreycey <albindreycey@gmail.com>
2021-11-09 23:59:12 -0700

    enrichseq DB UPDATER fixed

--- kraken_module/enrichseqDB_install.sh
+++ kraken_module/enrichseqDB_install.sh
@@ -8,90 +8,103 @@
 #     databases in the pipeline.
 #######
 
-
-
-
 # change to the correct directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$( dirname "${BASH_SOURCE[0]}" )" 
 
+
+
 ####
 # Description:
-#      This function uses ncbi-genome-download to download all 
-#      phage genomess into an output genome direrctory.
-# TODO:
-#      1. add the correct error and warnings.
+#      This function calls wget_file() to obtain all ftp DB required files.
+####
+function downloadFTPFiles() {
+  add_to_log "";
+  add_to_log "RUNNING: downloadFTPFiles()";
+  local dbDir=$1;
+  # Download phagesDB all phages database
+  actino_ftp='https://phagesdb.org/media/Actinobacteriophages-All.fasta';
+  wget_file ${actino_ftp} Actinobacteriophages-All.fasta Actinobacteriophages-All.fasta;
+  # Download NCBI taxonomy
+  taxdump_ftp='https://ftp.ncbi.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz';
+  wget_file ${taxdump_ftp} new_taxdump.tar.gz ${dbDir}/taxonomy/names.dmp;
+  # download accession to taxid for WGS
+  nucl_wgs_ftp='https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz';
+  wget_file ${nucl_wgs_ftp} nucl_gb.accession2taxid.gz ${dbDir}/taxonomy/nucl_gb.accession2taxid;
+}
+
+####
+# Description:
+#      This function uses ncbi-genome-download to download all phage genomes.
+# Notes:
+#      This function must be the first to add to ${genomeDir}
+# Errors:
+#      1. If ${genomeDir} not empty
 ####
 function NCBIPhageGenomeDownload() {
+  add_to_log "";
+  add_to_log "RUNNING: NCBIPhageGenomeDownload()";
+  add_to_log "NOTE: This step takes a while ... (typically about 30 minutes)";
   local genomeDir=$1;
+  COUNTER=1
+  MAX_ATTEMPTS=3
+  # ensure directory is empty
+  if [[ "$(ls -A ${genomeDir})" ]]; then
+    throw_warning "NCBIPhageGenomeDownload() ${genomeDir} SHOULD BE deleted before rerunning the script! Delete this then try again.";
+  fi
   # download NCBI phage genomes
-  while [[ ! -d refseq/viral ]]; do #TODO: ensure this is the right way to make while loop.
-    if [[ ! -d refseq/viral ]]; then
-      echo "Downloading NCBI phage genomes...";
-      #TODO: print out the attempt #
-      #TODO: If tries above 3, then exiit script and give advise for nternet connection
+  while [[ ! "$(ls -A ${genomeDir})" ]]; do
+    if [[ $COUNTER -gt $MAX_ATTEMPTS ]]; then break; fi;
+    if [[ ! "$(ls -A ${genomeDir})" ]]; then
+      # DOWNLOAD GENOMES
+      add_to_log "Downloading NCBI phage genomes...";
+      add_to_log "Attempt # ${COUNTER}";
       ncbi-genome-download  --formats fasta --assembly-level complete \
                             --genera phage --fuzzy-genus viral --parallel 4 \
                             --flat-output -o ${genomeDir}; 
-      # unzip the downloaded genomes
+      add_to_log "SUCCESS: genomes downloaded";
+      # UNZIP GENOMES
+      add_to_log "Unzipping the downloaded NCBI phage genomes...";
       find ref_genomes/ -name '*.gz' -exec gzip -d {} +;
+      add_to_log "SUCCESS: unzipping genomes worked";
     else 
-      # TODO break while loop, print "Success and output message"
       break
     fi
-  done
+    let COUNTER++
+  done;
 }
 
 ####
 # Description:
-#      This function calls wget_file() to obtain all ftp
-#      DB required files.
-####
-function DownloadFTPfiles() {
-  echo "Running DownloadFTPFiles()";
-  local dbDir=$1;
-  # Download phagesDB all phages database
-  actino_ftp='https://phagesdb.org/media/Actinobacteriophages-All.fasta'
-  wget_file ${actino_ftp} Actinobacteriophages-All.fasta Actinobacteriophages-All.fasta
-  # Download NCBI taxonomy
-  taxdump_ftp='https://ftp.ncbi.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz'
-  wget_file ${taxdump_ftp} new_taxdump.tar.gz ${dbDir}/taxonomy/names.dmp
-  # download accession to taxid for WGS
-  nucl_wgs_ftp='https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_wgs.accession2taxid.gz'
-  wget_file ${nucl_wgs_ftp} nucl_wgs.accession2taxid.gz ${dbDir}/taxonomy/nucl_wgs.accession2taxid 
-}
-
-####
-# Description:
-#      This function uses wget to obtain a file,
-#      and performs error handeling if the file
-#      is not corrrectly downloaded.
-# TODO:
-#      1. add the correct error and warnings.
+#      This function uses wget to obtain a file.
+# Errors:
+#      1. If file not downloaded after preset 25 times.
 ####
 function wget_file() {
+  add_to_log "";
+  add_to_log "RUNNING: wget_file()";
   local ftp_path=$1;
   local downloaded_file=$2;
   local final_file=$3;
-  while [[ ! -f ${downloaded_file} && ! -f ${final_file} ]]; do
-    if [[ ! -f ${downloaded_file} && ! -f ${final_file} ]]; then
-      # print out attempt number
-      echo; echo "      Downloading ${ftp_path}"; echo;
-      wget ${ftp_path};
-    else;
-      # TODO break while loop, print "Success and output message"
-      break
+  if [[ ! -f ${downloaded_file} && ! -f ${final_file} ]]; then
+    add_to_log "      Downloading ${ftp_path}";
+    wget --tries=25 ${ftp_path};
+    if [[ -f ${downloaded_file} ]]; then
+      add_to_log "SUCCESS: wget_file() downloaded ${downloaded_file}";
+    else
+      throw_fatal_error "wget_file() could not downlod ${downloaded_file}, try again (with internet!)";
     fi
-  done
+  else
+    add_to_log "wget_file() already downloaded ${downloaded_file}";
+  fi
 }
 
 ####
 # TODO:
-#      1. split function up into sub functions w/ error handeling.
+#      1. split function up into sub functions w/ error handling.
 ####
 function reorganizeFiles() {
   echo "Running reorganizeFiles()";
-  
   # arguments
   local genomeDir=$1;
   local dbDir=$2;
@@ -109,31 +122,14 @@ function reorganizeFiles() {
     moveFile nucl_gb.accession2taxid ${dbDir}/taxonomy;
   fi
 
+  # Reformat the NCBI files to compatible with kraken (and PathOrganizer)
+  python ${DIR}/ncbi2krakenHeader.py ${dbDir}/taxonomy/nucl_gb.accession2taxid ${genomeDir}
+  
   # Reformat ActinoDB file to be compatible with kraken
   python ${DIR}/kraken_db_format.py ${dbDir}/taxonomy/names.dmp \
          Actinobacteriophages-All.fasta ${actinoOutFile};
   moveFile ${actinoOutFile} ${genomeDir};
   rm Actinobacteriophages-All.fasta;
-}
-
-####
-# Description:
-#      Moves a file into a directory
-# Errors:
-#      1. If file empty
-#      2. If directory does not exist
-# Warnings:
-#      1. If directory already contains that file
-# TODO:
-#      1. add the correct error and warnings as above.
-####
-function moveFile() {
-  local file_to_move=$1;
-  local new_directory=$2;
-  echo "    moving the file ${file_to_move} to ${new_directory}";
-  if [[ -f ${file_to_move} ]]; then
-    mv ${file_to_move} ${new_directory}
-  fi
 }
 
 ####
@@ -144,17 +140,23 @@ function moveFile() {
 #      2. If ${genomeDir}/ empty
 # Warnings:
 #      1. if file without find extensions are in ${genomeDir}/
-# TODO:
-#      1. add the correct error and warnings as above.
 ####
 function addGenomesToDb() {
-  echo "Running addGenomesToDb()";
+  add_to_log "";
+  add_to_log "RUNNING: addGenomesToDb()";
   # arguments
   local genomeDir=$1;
   local dbDir=$2;
-  # add all fasta files in the genomes directory to the kraken library
-  find ${genomeDir}/ \( -name "*.fa" -o -name "*.fasta" -o -name "*.fna" \) \
-       -print0 | xargs -0 -I{} -n1 kraken2-build --add-to-library {} --db ${dbDir};
+  if ! [[ -x "$(command -v kraken2-build)" ]]; then 
+    throw_fatal_error "kraken2-build is missing, make sure environment for enrichseq is used. Read the README.md";
+  elif [[ ! "$(ls -A ${genomeDir})" ]]; then
+    throw_fatal_error "The genome directory is empty: ${genomeDir}";
+  else
+    # add all fasta files in the genomes directory to the kraken library
+    find ${genomeDir}/ \( -name "*.fa" -o -name "*.fasta" -o -name "*.fna" \) \
+        -print0 | xargs -0 -I{} -n1 kraken2-build --add-to-library {} --db ${dbDir};
+    add_to_log "SUCCESS: addGenomesToDb() added DB files from ${genomeDir}";
+  fi
 }
 
 ####
@@ -164,21 +166,15 @@ function addGenomesToDb() {
 #      1. If python kraken2-build missing
 ####
 function buildKrakenDb() {
-  echo "Running buildKrakenDb()";
+  add_to_log "";
+  add_to_log "RUNNING: buildKrakenDb()";
   # arguments
   local dbDir=$1;
-  kraken2-build --build --db ${dbDir}; # TODO: throw error if no kraken2-build
-}
-
-####
-# Description:
-#      This method makes a directory if it does not exist.
-####
-function makeDirectory() {
-  local directory_to_make=$1;
-  echo "making directory for ${directory_to_make}"
-  if [[ ! -d ${directory_to_make} ]]; then
-    mkdir ${directory_to_make};
+  if ! [[ -x "$(command -v kraken2-build)" ]]; then 
+    throw_fatal_error "kraken2-build is missing, make sure environment for enrichseq is used. Read the README.md";
+  else 
+    kraken2-build --build --db ${dbDir};
+    add_to_log "SUCCESS: buildKrakenDb() added files from ${dbDir}";
   fi
 }
 
@@ -193,54 +189,156 @@ function multifasta2fasta(){
   local multi_fasta_file=$1;
   local directory_for_fasta_files=$2;
   local multifasta2fasta_script=multifasta2single.py
-  echo "Turning ${multi_fasta_file} into multiple fasta files, then deleting";
+  add_to_log "";
+  add_to_log "RUNNING: multifasta2fasta()";
+  add_to_log "Turning ${multi_fasta_file} into multiple fasta files, then deleting";
   if [[ -f ${multifasta2fasta_script} ]]; then
-    if [[ -f ${multi_fasta_file} ]]; then
+    if [[ -f ${multi_fasta_file} && -s ${multi_fasta_file} ]]; then
       python ${DIR}/${multifasta2fasta_script} ${multi_fasta_file} ${directory_for_fasta_files};
+      add_to_log "SUCCESS: multifasta2fasta() finished creating fasta files from ${multi_fasta_file}";
       rm ${multi_fasta_file}; #TODO: Should the multifasta be deleted?
-    else;
-      throw_fatal_error "${multifasta2fasta_script} is missing from ${DIR}";
-  else; # ensure this is the correct use of else
-    throw_fatal_error "${multi_fasta_file} is missing or not found!!!!";
+      add_to_log "SUCCESS: multifasta2fasta() deleted ${multi_fasta_file}";
+    else
+      throw_fatal_error "${multifasta2fasta_script} is missing (or empty) from ${DIR}";
+    fi
+  else
+    throw_fatal_error "${multi_fasta_file} is missing or not found in ${DIR}";
   fi
 }
 
 ####
-# Description:
+# Description: 
+#      Deletes duplicate genomes in the genome directory
+# Errors:
+#      1. If db_cleaner.py not found
+####
+function delete_duplicates() {
+  add_to_log "";
+  add_to_log "RUNNING: delete_duplicates()";
+  local db_cleaner_py='db_cleaner.py'
+  local genome_dir=$1
+  if [[ ! -f ${DIR}/${db_cleaner_py} ]]; then
+    throw_fatal_error "${db_cleaner_py} is not found in ${DIR}! This must be in ${DIR} for the script to work!";
+  else
+    python ${db_cleaner_py} ${genome_dir}
+    add_to_log "SUCCESS: delete_duplicates() deleted duplicate genomes in ${genome_dir}";
+  fi
+}
+
+####
+# Description: (Helper Function)
+#      Moves a file into a directory
+# Errors:
+#      1. If file empty
+#      2. If directory does not exist
+# Warnings:
+#      1. If directory already contains that file
+####
+function moveFile() {
+  add_to_log "";
+  add_to_log "RUNNING: moveFile()";
+  add_to_log "    moving the file ${file_to_move} to ${new_directory}";
+  local file_to_move=$1;
+  local new_directory=$2;
+  if [[ ! -f ${file_to_move} ]]; then
+    throw_fatal_error "moveFile() the following FILE does not exist: ${file_to_move}";
+  elif [[ ! -d ${new_directory} ]]; then
+    throw_fatal_error "moveFile() the following DIR does not exist: ${new_directory}";
+  elif [[ -f ${new_directory}/${file_to_move} ]]; then
+    throw_warning "NOTE: moveFile() the file is already in the directory, adding new one...";
+    mv ${file_to_move} ${new_directory};
+    add_to_log "SUCCESS: moveFile() ${file_to_move} moved to ${new_directory}";
+  else
+    mv ${file_to_move} ${new_directory};
+    add_to_log "SUCCESS: moveFile() ${file_to_move} moved to ${new_directory}";
+  fi
+}
+
+####
+# Description: (Helper Function)
+#      This method makes a directory if it does not exist.
+####
+function makeDirectory() {
+  local directory_to_make=$1;
+  add_to_log "";
+  add_to_log "RUNNING: makeDirectory()";
+  add_to_log "making directory for ${directory_to_make}"
+  if [[ ! -d ${directory_to_make} ]]; then
+    mkdir ${directory_to_make};
+    add_to_log "SUCCESS: makeDirectory() made ${directory_to_make}";
+  else
+    throw_warning "NOTE: makeDirectory() already made ${directory_to_make}";
+  fi
+}
+
+####
+# Description: (Helper Function)
 #      This method throws a warning then continues.
 ####
 function throw_warning() {
   local warning_msg=$1
-  echo; echo "**WARNING:";
-  echo "${multifasta2fasta_script} is missing from ${DIR}";
+  echo; echo "**WARNING**:";
+  echo ${warning_msg};
+  add_to_log "**WARNING**: ${warning_msg}";
   echo;
 }
 
 ####
-# Description:
+# Description: (Helper Function)
 #      This method throws an error then exits.
 ####
 function throw_fatal_error() {
   local error_msg=$1
-  echo; echo "**ERROR:";
+  echo; echo "**ERROR**:";
   echo ${error_msg};
+  add_to_log "**ERROR**: ${error_msg}";
   echo "Exiting";
   exit 1; 
 }
 
+####
+# Description: (Helper Function)
+#      This function adds a message and time stamp to a log file.
+####
+function add_to_log() {
+  local msg=$1
+  DATE=`date "+%Y%m%d"`
+  DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`;
+  echo ${DATE_WITH_TIME} ${msg}
+  if [ ! -z ${LOG_FILE} ]; then
+    echo ${DATE_WITH_TIME} ${msg} >> ${LOG_FILE};
+  else
+    echo ${DATE_WITH_TIME} ${msg} >> DB_BUILD_${DATE}.log;
+  fi
+}
 
 ####
+# run_PIPELINE()
+# Description: The pipeline for building the database.
 ####
+function run_PIPELINE() {
+    makeDirectory ${genomeDir};
+    makeDirectory ${dbDir};
+    makeDirectory ${dbDir}/taxonomy;
+    downloadFTPFiles ${dbDir};
+    NCBIPhageGenomeDownload ${genomeDir};                                                
+    reorganizeFiles ${genomeDir} ${dbDir} ${actinoOutFile};                   
+    addGenomesToDb ${genomeDir} ${dbDir};                                       
+    buildKrakenDb ${dbDir};
+    multifasta2fasta ${movedActinoOutFile} ${genomeDir};
+    delete_duplicates ${genomeDir};
+}
+
+####
+# MAIN()
 # Description: The main function controls the flow of the script.
-####
 ####
 function main() {                                                               
   # input arguments                                                             
   if [[ -f ${DIR}/kraken_module.config ]]; then
     source ${DIR}/kraken_module.config;
   else
-    echo "The config file (kraken_module.config) is missing!";
-    exit 1;
+    throw_fatal_error "The config file (kraken_module.config) is missing!";
   fi
 
   # update arguments with full path
@@ -248,24 +346,53 @@ function main() {
   local dbDir=${DIR}/${dbDir};
   local movedActinoOutFile=${genomeDir}/${actinoOutFile}
   local actinoOutFile=${DIR}/${actinoOutFile};
-  echo $actinoOutFile;
 
-  # run the script                                           
-  if [[ ! -f ${dbDir}/taxo.k2d ]]; 
-  then   
-    makeDirectory ${genomeDir};
-    makeDirectory ${dbDir};
-    makeDirectory ${dbDir}/taxonomy;
-    NCBIPhageGenomeDownload ${genomeDir};
-    DownloadFTPFiles ${dbDir};                                                
-    reorganizeFiles ${genomeDir} ${dbDir} ${actinoOutFile};                   
-    addGenomesToDb ${genomeDir} ${dbDir};                                       
-    buildKrakenDb ${dbDir};
-    multifasta2fasta ${movedActinoOutFile} ${genomeDir};
+  # start log file
+  if [[ -f  ${LOG_FILE} ]]; then
+    add_to_log "The LOG file (${LOG_FILE}) already exists. If you'd like to rebuild, enter 'yes'";
+    read varname
+    if [[ $varname == 'yes' || $varname == 'y' ]]; then
+      rm ${LOG_FILE}
+      add_to_log "SUCCESS: deleted ${LOG_FILE}";
+    fi
+  fi
+
+  # Build genome directory     
+  if [[ "$(ls -A ${genomeDir})" ]]; then
+    add_to_log "The genome directory (${genomeDir}) already exists. If you'd like to rebuild, enter 'yes'";
+    read varname
+    if [[ $varname == 'yes' || $varname == 'y' ]]; then
+      rm -rf ${genomeDir}/
+      add_to_log "SUCCESS: deleted ${genomeDir}/";
+    fi
+  fi
+
+  # Build DB     
+  if [[ "$(ls -A ${dbDir})" ]]; then
+    add_to_log "The DB (${dbDir}) already exists. If you'd like to rebuild, enter 'yes'";
+    read varname
+    if [[ $varname == 'yes' || $varname == 'y' ]]; then
+      rm -rf ${dbDir}/
+      add_to_log "SUCCESS: deleted ${dbDir}/";
+    fi
+  fi
+
+  # run the DB building pipeline                                     
+  if [[ ! -f ${dbDir}/taxo.k2d ]]; then
+    run_PIPELINE;
   else
-    echo "The database was already built. If you'd like to rebuild, delete '${dbDir}/taxo.k2d'";
+    add_to_log "The database was already built. If you'd like to rebuild, enter 'yes'";
+    read varname
+    if [[ $varname == 'yes' || $varname == 'y' ]]; then
+      add_to_log "you said ${varname} - Rebuilding DB and rerunning the script!";
+      rm -rf ${dbDir}/
+      add_to_log "SUCCESS: deleted ${dbDir}/";
+      run_PIPELINE;
+    else 
+      add_to_log "you said ${varname} - Exiting since DB rebuild not desired";
+    fi
   fi                                                                            
 }
 
-echo "Running the kraken2 DB build script";
+echo; echo "Running the EnrichSeq database build script"; echo;
 main;
