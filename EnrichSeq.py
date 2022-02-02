9630b3f4a3ce2abb1c7cd849d055599b0501afe0
Dreycey Albin <33699024+Dreycey@users.noreply.github.com>
2022-02-02 16:05:14 -0700

    Update EnrichSeq.py
    ADDING F STRING

--- EnrichSeq.py
+++ EnrichSeq.py
@@ -87,7 +87,7 @@ def run_enrichseq(primary_args):
     if primary_args.genome_db:
         CMD_list += ["--genomedir", primary_args.genome_db]
     else:
-        CMD_list += ["--genomedir", "{CURR_PATH}/database/ref_genomes/"]
+        CMD_list += ["--genomedir", f"{CURR_PATH}/database/ref_genomes/"]
     CMD_list += ["--workdir", primary_args.output]
     out = utils.subproc_call(CMD_list)
     return out 
