d50a3617d2f851e7b7710674709e6de313514fcf
latifa <leotifa@gmail.com>
2022-02-24 18:10:19 -0700

    Adding kraken benchmarking (incomplete)

--- src/benchmarking/run_benchmarking.sh
+++ src/benchmarking/run_benchmarking.sh
@@ -20,7 +20,7 @@ function test_enrichseq() {
 }
 
 function test_viromexplorer() {
-    tool_name="FastViromeExplorer2";
+    tool_name="FastViromeExplorer";
     file_suffix=".fq";
     echo "TESTING ${tool_name}";
     for test_dir in ./tests/*; do
@@ -40,11 +40,34 @@ function test_viromexplorer() {
     done
 }
 
+function test_kraken2() {
+    tool_name="Kraken2";
+    file_suffix=".fa";
+    echo "TESTING ${tool_name}";
+    for test_dir in ./tests/*; do
+        if [[ -d $test_dir ]]; then
+            for file in $test_dir/*${file_suffix}; do
+               echo; echo $file; echo;
+               basefile="$(basename -- $file)";
+               test_dir_name="$(basename "${test_dir##*/}")";
+               mkdir -p results/${tool_name}/${test_dir_name}/;
+               kraken2 --use-names --threads 4 --db ${dbDir} --report ${reportFile} ${inFasta} > ${krakenOutFile}
+           	   #java -cp tools/FastViromeExplorer/bin FastViromeExplorer \
+               #                                         -1 $file \
+               #                                         -i tools/FastViromeExplorer/ncbi-virus-kallisto-index-k31.idx \
+               #                                         -o results/${tool_name}/${test_dir_name}/${basefile%${file_suffix}} \
+               #                                         -l tools/FastViromeExplorer/ncbi-viruses-list.txt; 
+    	    done
+    	fi
+}
+
 function main() {
     mkdir results/;
     conda activate enrichseq;
-    test_enrichseq;
-    conda activate FastViromeExplorer;
-    test_viromexplorer;
+    #test_enrichseq;
+    test_kraken2;
+
+    #conda activate FastViromeExplorer;
+    #test_viromexplorer;
 }
 main
